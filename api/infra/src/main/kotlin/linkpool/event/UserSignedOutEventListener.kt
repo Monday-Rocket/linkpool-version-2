85bf873f4bdb77bf85277fc6b349b3a880cea25b
HyeonWuJeon <tkawnd111@gmail.com>
2023-08-28 09:35:41 +0900

    [issue-1] refactor: async event listener

--- api/infra/src/main/kotlin/linkpool/event/UserSignedOutEventListener.kt
+++ api/infra/src/main/kotlin/linkpool/event/UserSignedOutEventListener.kt
@@ -1,15 +1,23 @@
 package linkpool.event
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import linkpool.common.log.kLogger
 import linkpool.folder.port.`in`.FolderEventListener
 import linkpool.link.port.`in`.LinkEventListener
 import linkpool.user.model.UserSignedOutEvent
 import org.springframework.integration.annotation.ServiceActivator
 import org.springframework.messaging.Message
 import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
+
+fun <T> CoroutineScope.safeAsync(block: suspend () -> T): Deferred<Result<T>> {
+    return async {
+        runCatching {
+            block()
+        }
+    }
+}
 
 @Component
 class UserSignedOutEventListener(
@@ -17,11 +25,24 @@ class UserSignedOutEventListener(
     private val linkEventListener: LinkEventListener
 ) {
 
+    private val log = kLogger()
+
+    @Async
     @ServiceActivator(inputChannel = "signedOutEvent")
     fun handleForSignedOutEventListener(@Payload message: Message<UserSignedOutEvent>) {
         CoroutineScope(Job()).launch {
-            folderEventListener.deleteBatchAll(message.payload)
-            linkEventListener.deleteBatchAll(message.payload)
+            val folderEventDeferred = safeAsync {
+                folderEventListener.deleteBatchAll(message.payload)
+            }
+            val linkEventDeferred = safeAsync {
+                linkEventListener.deleteBatchAll(message.payload)
+            }
+            folderEventDeferred.await()
+                .onSuccess { log.info("succeed in folderEventListener from UserSignedOut... userId: ${message.payload.userId}") }
+                .onFailure { e -> log.info("Error in folderEventListener from UserSignedOut... userId: ${message.payload.userId} \n error: ${e.message}") }
+            linkEventDeferred.await()
+                .onSuccess { log.info("succeed in linkEventListener from UserSignedOut... userId: ${message.payload.userId}") }
+                .onFailure { e -> log.info("Error in linkEventListener from UserSignedOut... userId: ${message.payload.userId} \n error: ${e.message}") }
         }
     }
 }
